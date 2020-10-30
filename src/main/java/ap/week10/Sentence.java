4888c6e206d4c66466166fb90d3f8223fab6a8f0
Alexander Taylor <awtpi314@gmail.com>
2020-10-29 19:28:10 -0500

    Added sentence test and finished sentence class

--- src/main/java/ap/week10/Sentence.java
+++ src/main/java/ap/week10/Sentence.java
@@ -1,5 +1,46 @@
 package ap.week10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sentence {
-    
+    private String sentence;
+
+    public Sentence(String s) {
+        setSetence(s);
+    }
+
+    public void setSetence(String s) {
+        sentence = s;
+    }
+
+    public List<Integer> getBlankPositions() {
+        List<Integer> positions = new ArrayList<>();
+        int index = sentence.indexOf(' ');
+        while (index != -1) {
+            positions.add(index);
+            index = sentence.indexOf(' ', index + 1);
+        }
+        return positions;
+    }
+
+    public int countWords() {
+        if (sentence == null || sentence.equals("")) {
+            return 0;
+        }
+
+        int count = 1;
+
+        int index = sentence.indexOf(' ');
+        while (index != -1) {
+            count++;
+            index = sentence.indexOf(' ', index + 1);
+        }
+
+        return count;
+    }
+
+    public String[] getWords() {
+        return sentence.split(" ");
+    }
 }
